6732be685d9e0ba9f7305bc52d3d06799ceced5e
Suberror27 <abrahamct27@gmail.com>
2025-06-02 22:42:12 -0500

    JavaScript - Sub Unit Unary Operators

---
+++ JavaScript/19 - Unary Operators/app.js
@@ -0,0 +1,17 @@
+let score  = 10;
+
+score = score + 5;
+
+// Same as core = score + 5;
+score += 5;
+
+// Same as score + 1
+score ++;
+
+// Same as score - 1
+score --;
+
+const bonusMult = 3;
+
+// Smame as score * bonusMult
+score *= bonusMult;
