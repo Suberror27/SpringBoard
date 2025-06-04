3fb4f31691426ff68bdf853b44ce0bbe99c6be2d
Suberror27 <abrahamct27@gmail.com>
2025-06-03 21:57:55 -0500

    JavaScript Working with Primitives - Sub Unit String Methods

---
+++ JavaScript/Working with Primitives/9 - String Methods/app.js
@@ -0,0 +1,13 @@
+const stringToCheck = " Abraham_Camargo_26 ";
+
+const stringToCheckUppercase = stringToCheck.toUpperCase();
+
+const stringToCheckLowercase = stringToCheck.toLowerCase();
+
+const stringToCheckTrimed = stringToCheck.trim();
+
+console.log(stringToCheckUppercase); // Logs ABRAHAM_CAMARGO_26
+
+console.log(stringToCheckLowercase); // Logs abraham_camargo_26
+
+console.log(stringToCheckTrimed); // Logs Abraham_Camargo_26 without any empty spaces
