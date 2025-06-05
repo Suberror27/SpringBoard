945a6814671a6da02d1f555b8a140b3babcab965
Suberror27 <abrahamct27@gmail.com>
2025-06-04 21:31:09 -0500

    JavaScript Working with Primitives - Sub Unit Null & Undefined

---
+++ JavaScript/Working with Primitives/21 - Null & Undefined/app.js
@@ -0,0 +1,12 @@
+let loggedInUser = null;
+
+loggedInUser = "Abraham";
+
+if (loggedInUser !== null) console.log("Somebody is logged in!");
+else console.log("Nobody logged in!");
+
+let u = undefined;
+let t;
+
+console.log(u); // Logs undefined
+console.log(t); // Logs undefined
