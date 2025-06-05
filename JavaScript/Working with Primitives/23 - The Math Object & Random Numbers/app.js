1c5b3d2ceb691238daaae2291f087dcc50ccf09b
Suberror27 <abrahamct27@gmail.com>
2025-06-04 21:43:28 -0500

    JavaScript Working with Primitives - Sub Unit The Math Object & Random Numbers

---
+++ JavaScript/Working with Primitives/23 - The Math Object & Random Numbers/app.js
@@ -0,0 +1,23 @@
+// console.log(Math.PI); //3.14
+
+// console.log(Math.round(4.8)); //5
+
+// console.log(Math.abs(-1234)); //1234
+
+// console.log(Math.pow(2,5)); //32
+
+// console.log(Math.floor(3.8989898)); //3
+
+// console.log(Math.random()); //Random decimal between 0 and 1
+
+const step1 = Math.random();
+
+const step2 = step1 * 10;
+
+const step3 = Math.floor(step2);
+
+const step4 = step3 + 1;
+
+console.log(step4);
+
+console.log(Math.floor(Math.random() * 10) + 1);
