6c5322c2f73d541a637f045c7c1ec73af879b2a6
Suberror27 <abrahamct27@gmail.com>
2025-06-04 21:56:29 -0500

    JavaScript Working with Primitives - Sub Unit parseInt & parseFloat

---
+++ JavaScript/Working with Primitives/27 - parseInt & parseFloat/app.js
@@ -0,0 +1,11 @@
+console.log(parseInt("26"));
+
+console.log(parseInt("26.12"));
+
+console.log(parseInt("28DaysLater"));
+
+console.log(parseFloat("26.12"));
+
+console.log(parseFloat("7"));
+
+console.log(parseFloat("I ate 6 shrimps"));
