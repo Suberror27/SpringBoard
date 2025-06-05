03a474bc12f03911d7a5a68a5007a9ea3d67c8f8
Suberror27 <abrahamct27@gmail.com>
2025-06-04 21:49:56 -0500

    JavaScript Working with Primitives - Sub Unit Typeof Operator

---
+++ JavaScript/Working with Primitives/25 - Typeof Operator/app.js
@@ -0,0 +1,12 @@
+const number = 26;
+const string = "Abraham Camargo";
+const boolean = true;
+const isNull = null;
+const isUndefined = undefined;
+
+
+console.log(typeof(number));
+console.log(typeof(string));
+console.log(typeof(boolean));
+console.log(typeof(isNull));
+console.log(typeof(isUndefined))
