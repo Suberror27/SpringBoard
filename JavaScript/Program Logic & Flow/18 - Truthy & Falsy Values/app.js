f37941be97788b7574442bbd9e8487cfec47d574
Suberror27 <abrahamct27@gmail.com>
2025-06-06 22:44:19 -0500

    JavaScript Program Logix & Flow - Sub Unit Truthy & Falsy Values

---
+++ JavaScript/Program Logic & Flow/18 - Truthy & Falsy Values/app.js
@@ -0,0 +1,15 @@
+// let mystery = 5;         Truthy!
+// let mystery = true;      Truthy!
+// let mystery = -1;        Truthy!
+// let mystery = 0;         Falsy!
+// let mystery = NaN;       Falsy!
+// let mystery = false;     Falsy!
+// let mystery = "";        Falsy!
+// let mystery = "Abraham"; Truthy!
+// let mystery = "-A"; T    ruthy!
+
+if (mystery) {
+    console.log("Truthy!");
+} else {
+    console.log("Falsy!")
+};
