7b80aa8fd3b0871c4b1483c6d86205e8fad9eed1
Suberror27 <abrahamct27@gmail.com>
2025-06-06 22:12:32 -0500

    JavaScript Program Logix & Flow - Sub Unit Comparison Operators

---
+++ JavaScript/Program Logic & Flow/4 - Comparison Operators/app.js
@@ -0,0 +1,21 @@
+const valueA = 27;
+const valueB = 15;
+const valueC = 9;
+
+if (valueA > valueB) {
+    console.log("Value A is greater!")
+} else {
+    console.log("Value B is greater!")
+}
+
+if (valueC < valueB) {
+    console.log("ValueC is not lower");
+} else {
+    console.log("ValueC is lower!");
+};
+
+if (valueA >= valueB) {
+    console.log("ValueA is greater or equal to ValueB")
+} else {
+    console.log("ValueA is not greater or equal to ValueB")
+}
