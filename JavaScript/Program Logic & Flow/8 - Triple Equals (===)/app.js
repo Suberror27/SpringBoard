6eae2cd70dd2e8989fee77ef652cb7ef3d6a118e
Suberror27 <abrahamct27@gmail.com>
2025-06-06 22:24:20 -0500

    JavaScript Program Logix & Flow - Sub Unit Triple Equals (===)

---
+++ JavaScript/Program Logic & Flow/8 - Triple Equals (===)/app.js
@@ -0,0 +1,14 @@
+const number1 = 1;
+const numberA = "1";
+const number2 = 2;
+const numberC = "3";
+
+console.log(number1 === numberA);
+
+console.log(number1 !== numberA);
+
+console.log(number2 === numberC);
+
+console.log(null === undefined);
+
+console.log(0 === "");
