eb835107ee898de9ab3ff5a34bfb9eedd0ab00e0
Suberror27 <abrahamct27@gmail.com>
2025-06-09 22:33:38 -0500

    JavaScript Program Logix & Flow - Sub Unit Operator Precedence

---
+++ JavaScript/Program Logic & Flow/26 - Operator Precedence/app.js
@@ -0,0 +1,32 @@
+// const x = 7;
+
+// console.log(Boolean(x == 7 || x === 3 && x > 10));
+
+const expression = (7 - (3 * 2)) + 5;
+
+const result = expression;
+
+console.log(result);
+
+// Operator Precedence:
+// Multiplication (*) comes before addition/subtraction.
+
+// So we do:
+
+// 3 * 2 = 6
+
+// Now the expression is:
+
+// 7 - 6 + 5
+
+// Evaluate left to right:
+
+// 7 - 6 = 1
+
+// 1 + 5 = 6
+
+const expression2 = (9 / 3) + (2 * 4);
+
+const result2 = expression2;
+
+console.log(result2);
