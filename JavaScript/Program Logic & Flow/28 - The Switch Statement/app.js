ca36b0edc6ae158b59943e47e1646f1c07a65760
Suberror27 <abrahamct27@gmail.com>
2025-06-09 22:47:05 -0500

    JavaScript Program Logix & Flow - Sub Unit The Switch Statement

---
+++ JavaScript/Program Logic & Flow/28 - The Switch Statement/app.js
@@ -0,0 +1,47 @@
+const dayOfWeek = 10;
+
+// if (dayOfWeek === 1) {
+//     console.log("MONDAY");
+// } else if (dayOfWeek === 2) {
+//     console.log("TUESDAY");
+// } else if (dayOfWeek === 3) {
+//     console.log("WEDNESDAY");
+// } else if (dayOfWeek === 4) {
+//     console.log("THURSDAY");
+// } else if (dayOfWeek === 5) {
+//     console.log("FRIDAY");
+// } else if (dayOfWeek === 6) {
+//     console.log("SATURDAY");
+// } else if (dayOfWeek === 7) {
+//     console.log("SUNDAY");
+// } else {
+//     console.log("INVALID DAY")
+// };
+
+switch(dayOfWeek) {
+    case 1:
+        console.log("MONDAY");
+    break;
+    case 2:
+        console.log("TUESDAY");
+    break;
+    case 3:
+        console.log("WEDNESDAY");
+    break;
+    case 4:
+        console.log("THURSDAY");
+    break;
+    case 5:
+        console.log("FRIDAY");
+    break;
+    case 6:
+        console.log("SATURDAY");
+    break;
+    case 7:
+        console.log("SUNDAY");
+    break;
+    default:
+        console.log("INVALID DAY")
+    break;
+};
+
