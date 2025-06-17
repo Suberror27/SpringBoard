3285d9ecd3da4f6c461e4c1ab3129ecdbd0db384
Suberror27 <abrahamct27@gmail.com>
2025-06-16 20:36:38 -0500

    JavaScript Objects - Sub Unit Nested Arrays & Objects

---
+++ JavaScript/Objects/10 - Nested Arrays & Objects/app.js
@@ -0,0 +1,47 @@
+// Object literal
+const testObject = {
+    name: "Abraham Camargo",
+    age: 26,
+    jobTitle: "Data Systems Administrator",
+    company: "Walmart",
+    currentlyEmployed: true,
+    isAdmin: false,
+    100: "one hundred",
+    10: "ten",
+    "my first name": "Abraham",
+    nested: {
+        testArray: [1, 2, 3],
+        testObject: {
+            "one": 1,
+            "two": 2,
+            "three": 3,
+        },
+    },
+};
+
+const colorObject = {
+    red: "#eb4d4b",
+    yellow: "#f9ca24",
+    blue: "#30336b"
+}
+
+const game = {
+    player1: {
+        userName: "Blue",
+        team: "O"
+    },
+    player2: {
+        userName: "Red",
+        team: "X"
+    },
+    board: [
+        ["O", null, "X"],
+        ["X", null, "O"],
+        [null, "X", "O"],
+    ],
+}
+
+console.log(testObject["nested"].testArray[2]);
+console.log(testObject.nested.testObject["one"]);
+
+console.log(game);
