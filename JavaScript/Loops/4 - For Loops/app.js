f99c6dec528f1df2486837655eaf2f536143a3d0
Suberror27 <abrahamct27@gmail.com>
2025-06-19 22:17:16 -0500

    JavaScript Loops - Sub Unit For Loops

---
+++ JavaScript/Loops/4 - For Loops/app.js
@@ -0,0 +1,9 @@
+// const someNumber = 20;
+
+// for (let i = 1; i <= someNumber; i++) {
+//     console.log(`${i} * ${i} = ${i * i}`);
+// };
+
+for (let a = 50; a <= 0; a -= 10){
+    console.log("Hello" + a);
+};
