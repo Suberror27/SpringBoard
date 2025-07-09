5c5097b9581b965bb8626f32039f2044cb1df517
Suberror27 <abrahamct27@gmail.com>
2025-07-09 15:00:34 -0500

    DOM Manipulation JavaScript Events - Load Events

---
+++ DOM Manipulation/JavaScript Events/5 - Load Events/app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // console.log(document.querySelectorAll("img"));
+    console.log("DOM Content Loaded");
+    // const newButton = document.querySelector("#newButton");
+
+
+    // newButton.addEventListener("click", () => {
+    //     alert("Button was clicked!");
+    // });
+}) 
+
+window.addEventListener("load", () => {
+    console.log("Fully Loaded");
+})
+
